50dfc195a9ba26206da14670dfb25081044cb411
eynimeni <maganatte@yahoo.de>
2022-06-30 11:23:28 +0200

    chat button showing title

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  public title = 'Chat';
+  public subTitle = `von ${new Date().toISOString()}`;
+  public titleVisible = false;
+  //public names = ['Max Mustermann', 'John Doe', 'Jane Doe', 'Hans Wurst', 'Frederike Feuerrot'];
 
   constructor() {}
 
+  public sendMessage():void {
+    this.titleVisible = !this.titleVisible
+  }
+
+ /* public handleButtonClick():void {
+    //this.title = 'New angular demo changed at' + new Date().toISOString();
+    this.title = `New angular demo changed at ${new Date().toISOString()}`;
+    this.subTitleVisible = !this.subTitleVisible;
+  }*/
 }
