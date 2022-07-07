f357ff6a65f0e22a8c54f7a093fb898b952e8431
eynimeni <maganatte@yahoo.de>
2022-07-07 13:56:37 +0200

    all requirements met!

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { GlobalChatStorageService } from "../services/global-chat-storage.service";
 import { Chat} from "../../models/chat model";
+import {Route} from "@angular/router";
+import {Tab2Page} from "../tab2/tab2.page";
+import {IonTabs} from "@ionic/angular";
 
 @Component({
   selector: 'app-tab1',
@@ -8,21 +11,21 @@ import { Chat} from "../../models/chat model";
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  public chatDisplayTab1 = false;
   public chats = new Array<Chat>();
 
-  constructor(protected globalChatStorageService: GlobalChatStorageService) {}
+  constructor(protected globalChatStorageService: GlobalChatStorageService, protected tab: IonTabs) {}
 
   startChat() {
+    this.tab.select("tab2");
 
-    console.log("chat started")
-    //hier zu tab 2 wechseln
   }
 
   public async viewChats(): Promise<void> {
-    this.chatDisplayTab1 = true
     this.chats = await this.globalChatStorageService.get()
 
     console.log(this.chats)
+
+    //todo Beim Auswählen eines vergangenen Chats, sieht man den gesamten Verlauf
+    //momentan wird noch nicht ausgewählt
   }
 }
